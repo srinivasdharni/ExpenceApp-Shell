1635bb6860464cdde508fc8997ac0d0666e11a7b
India <India@India-PC>
2023-09-09 21:16:34 +0530

    modified

--- mysql.sh
+++ mysql.sh
@@ -2,20 +2,21 @@ source common.sh
 
 echo disable mysql 8 version
 dnf module disable mysql -y &>>$log_file
-echo $?
+start_check
 
 echo copy mysql repo file
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
-echo $?
+start_check
 
 echo install mysql server
 dnf install mysql-community-server -y &>>$log_file
-echo $?
+start_check
 
 echo start mysql service
 systemctl enable mysqld &>>$log_file
 systemctl start mysqld &>>$log_file
-echo $?
+start_check
 
 echo setup root password
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
+mysql_root_password=$1
+mysql_secure_installation --set-root-pass $mysql_root_password &>>$log_file
