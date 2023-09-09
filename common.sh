ea7bc0882e0aed76582722184c95463c9f2f90d5
India <India@India-PC>
2023-09-09 20:49:14 +0530

    modified

--- common.sh
+++ common.sh
@@ -4,11 +4,11 @@ download_and_extract(){
 
 echo Download $component code
 curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >>$log_file
-echo $?
+start_check
 
 echo extracting $component code
 unzip /tmp/$component.zip >>$log_file
-echo $?
+start_check
 
 }
 
