1089d6cc64645096baad9c741309ee747ddb68c6
India <India@India-PC>
2023-09-09 14:05:08 +0530

    modified

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,21 @@
 echo Installing nginx
-dnf install nginx -y >>/tmp/expense.log
+log_file=/tmp/expense.log
+dnf install nginx -y >>log_file
 
 echo placing expense config file in nginx
-cp expense.conf /etc/nginx/default.d/expense.conf >>/tmp/expense.log
+cp expense.conf /etc/nginx/default.d/expense.conf >>log_file
 
 echo removing old nginx content
-rm -rf /usr/share/nginx/html/* >>/tmp/expense.log
+rm -rf /usr/share/nginx/html/* >>log_file
 
 echo Download frontend code
-curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>/tmp/expense.log
+curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>log_file
 
 cd /usr/share/nginx/html 
 
 echo extracting frontend code
-unzip /tmp/frontend.zip >>/tmp/expense.log
+unzip /tmp/frontend.zip >>log_file
 
 echo start nginx serive
-systemctl enable nginx >>/tmp/expense.log
-systemctl restart nginx >>/tmp/expense.log
+systemctl enable nginx >>log_file
+systemctl restart nginx >>log_file
