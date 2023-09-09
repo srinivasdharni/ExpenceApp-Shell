73518ce9770a0f86cb263c1b266ed419677de7a3
India <India@India-PC>
2023-09-09 14:34:14 +0530

    commit

--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,17 @@
-dnf module disable mysql -y 
+log_file=/tmp/expense.log
 
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo disable mysql 8 version
+dnf module disable mysql -y >>$log_file
 
-dnf install mysql-community-server -y
+echo copy mysql repo file
+cp mysql.repo /etc/yum.repos.d/mysql.repo >>$log_file
 
-systemctl enable mysqld
-systemctl start mysqld  
+echo install mysql server
+dnf install mysql-community-server -y >>$log_file
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
+echo start mysql service
+systemctl enable mysqld >>$log_file
+systemctl start mysqld >>$log_file
+
+echo setup root password
+mysql_secure_installation --set-root-pass ExpenseApp@1 >>$log_file
